f49b95665ac4559816ef25e9a4230faebb7b4db7
Bacto <adrien@bacto.net>
2018-12-17 11:12:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mercure Hub: getting started
+
+![Screenshot](./screenshot.png)
+
+First you have to create a Mercure Hub service on Stackhero.
+
+In Stackhero's configuration, allow "anonymous subscribers" to let the client connect to topics without authentication.
+
+
+## Back end
+
+Fill the configuration informations in `backend/.env-example` and rename the file to `.env`.
+Then start the server with `npm run start`.
+
+
+## Front end
+
+In `frontend/susbcriber.html`, fill the `endpoint` (domain name of Stackhero's service).
+
+Then open the file `susbcriber.html` in your browser.
+
+The back end code will send datas every seconds to the Mercure Hub, on a the topic `/books/1`.
+The frond end will listen datas from the topic `/books/1` and display them.
+
+
+Congratulations, you have a Mercure Hub up and running! 🎉
---
+++ backend/app.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const request = require('request-promise-native');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+if (!process.env.MERCURE_PUBLISHER_JWT_KEY) {
+  throw Error('You should first fill the .env-example file and the rename it to .env');
+}
+
+console.log('You should open the file subscriber.html to see the datas being dispatched.');
+
+const endpoint = process.env.MERCURE_SERVER;
+const publisherJwtKey = process.env.MERCURE_PUBLISHER_JWT_KEY;
+
+(async () => {
+  while (true) {
+    // Defining the topic where we want to dispatch the new data
+    const topic = `https://${endpoint}` + '/books/1';
+
+    // Defining a random stock count and prepare the data
+    const stockCount = Math.floor(Math.random() * (100 - 1) + 1);
+    const data = { topic, stockCount, available: true };
+
+
+    // Generating the bearer (JWT)
+    const bearer = jwt.sign(
+      { mercure: { publish: [ topic ] } },
+      publisherJwtKey,
+      { expiresIn: 60 } // Bearer expiring in one minute
+    );
+
+
+    // Sending the datas to Mercure Hub that will dispatch them to clients
+    console.log(`Sending datas: ${JSON.stringify(data)}`);
+    await request.post(
+      {
+        url: `https://${endpoint}/hub`,
+        auth: { bearer },
+        form: { topic, data: JSON.stringify(data) },
+        agentOptions: { rejectUnauthorized: false } // Just for debug, should always be set to true or commented!
+      }
+    );
+
+    await delay(1000);
+  }
+})().catch(error => {
+  console.error('');
+  console.error('🐞 An error occurred!');
+  console.error(error);
+  process.exit(1);
+});
